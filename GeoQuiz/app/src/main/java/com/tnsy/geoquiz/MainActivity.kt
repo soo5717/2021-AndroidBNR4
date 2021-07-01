4ce8797b7f02f2fe1ef4c6c7072d3b7491c43f82
suyeon <soo5717@sookmyung.ac.kr>
2021-07-02 03:56:16 +0900

    feat(GeoQuiz): Ch02 챌린지 - Previous 버튼 추가
    - Previous 버튼을 추가해 이전 질문을 볼 수 있게 함.

--- GeoQuiz/app/src/main/java/com/tnsy/geoquiz/MainActivity.kt
+++ GeoQuiz/app/src/main/java/com/tnsy/geoquiz/MainActivity.kt
@@ -2,6 +2,7 @@ package com.tnsy.geoquiz
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -10,7 +11,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var nextButton: Button
+    private lateinit var previousButton: ImageButton
+    private lateinit var nextButton: ImageButton
     private lateinit var questionTextView: TextView
 
     private val questionBank = listOf(
@@ -30,6 +32,7 @@ class MainActivity : AppCompatActivity() {
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
+        previousButton = findViewById(R.id.previous_button)
         nextButton = findViewById(R.id.next_button)
         questionTextView = findViewById(R.id.question_text_view)
 
@@ -43,6 +46,16 @@ class MainActivity : AppCompatActivity() {
         // 챌린지1 : TextView에 리스너 추가
         questionTextView.setOnClickListener {
             currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
+        // 챌린지2 : Previous 버튼 추가
+        previousButton.setOnClickListener {
+            currentIndex = if (currentIndex == 0) {
+                questionBank.size - 1
+            } else {
+                (currentIndex + 1) % questionBank.size
+            }
             updateQuestion()
         }
 
