0382ee9953b3f7fe3b3cbfd22cc835f0870b79a6
suyeon <soo5717@sookmyung.ac.kr>
2021-07-03 08:23:37 +0900

    feat(GeoQuiz): Ch04 회전 시 UI 상태 유지
    - ViewModel을 활용하여 회전 시에도 UI 상태가 유지되도록 함.

--- GeoQuiz/app/src/main/java/com/tnsy/geoquiz/MainActivity.kt
+++ GeoQuiz/app/src/main/java/com/tnsy/geoquiz/MainActivity.kt
@@ -12,31 +12,20 @@ private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
+    // ViewModel 연결
+    private val quizViewModel: QuizViewModel by lazy {
+        ViewModelProvider(this).get(QuizViewModel::class.java)
+    }
+
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
     private lateinit var nextButton: Button
     private lateinit var questionTextView: TextView
 
-    private val questionBank = listOf(
-        Question(R.string.question_australia, true),
-        Question(R.string.question_oceans, true),
-        Question(R.string.question_mideast, false),
-        Question(R.string.question_africa, false),
-        Question(R.string.question_americas, true),
-        Question(R.string.question_asia, true)
-    )
-
-    private var currentIndex = 0
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
-
-        // ViewModel 연결
-        val quizViewModel =ViewModelProvider(this).get(QuizViewModel::class.java)
-        Log.d(TAG, "Got a QuizViewModel: $quizViewModel")
-
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
@@ -51,7 +40,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         nextButton.setOnClickListener {
-            currentIndex = (currentIndex + 1) % questionBank.size
+            quizViewModel.moveToNext()
             updateQuestion()
         }
 
@@ -84,12 +73,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun updateQuestion() {
-        val questionTextResId = questionBank[currentIndex].textResId
+        val questionTextResId = quizViewModel.currentQuestionText
         questionTextView.setText(questionTextResId)
     }
 
     private fun checkAnswer(userAnswer: Boolean) {
-        val correctAnswer = questionBank[currentIndex].answer
+        val correctAnswer = quizViewModel.currentQuestionAnswer
 
         val messageResId = if (userAnswer == correctAnswer) {
             R.string.correct_toast
--- GeoQuiz/app/src/main/java/com/tnsy/geoquiz/QuizViewModel.kt
+++ GeoQuiz/app/src/main/java/com/tnsy/geoquiz/QuizViewModel.kt
@@ -1,18 +1,29 @@
 package com.tnsy.geoquiz
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 
 private const val TAG = "QuizViewModel"
 
 class QuizViewModel : ViewModel() {
 
-    init {
-        Log.d(TAG, "ViewModel instance created")
-    }
+    private var currentIndex = 0
 
-    override fun onCleared() { // ViewModel이 소멸되기 직전에 호출 됨.
-        super.onCleared()
-        Log.d(TAG, "ViewModel instance about to be destroyed")
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+
+    val currentQuestionAnswer: Boolean
+        get() = questionBank[currentIndex].answer
+
+    val currentQuestionText: Int
+        get() = questionBank[currentIndex].textResId
+
+    fun moveToNext() {
+        currentIndex = (currentIndex + 1) % questionBank.size
     }
 }
