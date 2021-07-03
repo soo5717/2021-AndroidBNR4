d87e6de0a3823906bfdb51554915d10a0357bbbd
suyeon <soo5717@sookmyung.ac.kr>
2021-07-03 10:29:27 +0900

    feat(GeoQuiz): Ch06 커닝 액티비티 생성 및 레이아웃 구성
    - 커닝 액티비티 생성 및 레이아웃 구성
    - 커닝 관련 문자열 리소스 추가

---
+++ GeoQuiz/app/src/main/java/com/tnsy/geoquiz/CheatActivity.kt
@@ -0,0 +1,11 @@
+package com.tnsy.geoquiz
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CheatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cheat)
+    }
+}
