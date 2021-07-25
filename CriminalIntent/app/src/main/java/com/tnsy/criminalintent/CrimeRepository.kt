7b7dd2fffe615ed7236a9bc9b705dc88b515c00c
suyeon <soo5717@sookmyung.ac.kr>
2021-07-25 19:52:27 +0900

    feat(CriminalIntent): Ch11 Repository 속성 설정 및 함수 추가
    - CrimeDatabase Builder 설정
    - CrimeDao 함수 추가

--- CriminalIntent/app/src/main/java/com/tnsy/criminalintent/CrimeRepository.kt
+++ CriminalIntent/app/src/main/java/com/tnsy/criminalintent/CrimeRepository.kt
@@ -1,8 +1,27 @@
 package com.tnsy.criminalintent
 
 import android.content.Context
+import androidx.room.Room
+import com.tnsy.criminalintent.database.CrimeDatabase
+import java.util.*
+
+private const val DATABASE_NAME = "crime-database"
 
 class CrimeRepository private constructor(context: Context){ // 싱글톤
+
+    // 데이터베이스 생성
+    private val database : CrimeDatabase = Room.databaseBuilder(
+        context.applicationContext,
+        CrimeDatabase::class.java,
+        DATABASE_NAME
+    ).build()
+
+    private val crimeDao = database.crimeDao()
+
+    fun getCrimes(): List<Crime> = crimeDao.getCrimes()
+
+    fun getCrime(id: UUID): Crime? = crimeDao.getCrime(id)
+
     companion object {
         private var INSTANCE: CrimeRepository? = null
 
