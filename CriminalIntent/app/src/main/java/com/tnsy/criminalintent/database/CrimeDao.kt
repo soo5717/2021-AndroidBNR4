9a61a31889f093324656d049a5c41727e5880151
suyeon <soo5717@sookmyung.ac.kr>
2021-08-01 17:44:28 +0900

    feat(CriminalIntent): Ch11 LiveData 사용하기
    - DAO 반환 타입 LiveData로 변경
    - CrimeListFragment 로직 LiveData에 맞게 변경

--- CriminalIntent/app/src/main/java/com/tnsy/criminalintent/database/CrimeDao.kt
+++ CriminalIntent/app/src/main/java/com/tnsy/criminalintent/database/CrimeDao.kt
@@ -1,5 +1,6 @@
 package com.tnsy.criminalintent.database
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Query
 import com.tnsy.criminalintent.Crime
@@ -8,9 +9,10 @@ import java.util.*
 @Dao
 interface CrimeDao {
 
+    // 백그라운드 스레드에서 쿼리 실행 후 LiveData 반환
     @Query("SELECT * FROM crime")
-    fun getCrimes(): List<Crime>
+    fun getCrimes(): LiveData<List<Crime>>
 
     @Query("SELECT * FROM crime WHERE id=(:id)")
-    fun getCrime(id: UUID): Crime?
+    fun getCrime(id: UUID): LiveData<Crime?>
 }
