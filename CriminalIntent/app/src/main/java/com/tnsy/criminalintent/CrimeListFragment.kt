4ca4ae45b102e6da1656ba5f6d64ca2a0f6d65f7
suyeon <soo5717@sookmyung.ac.kr>
2021-08-01 19:51:46 +0900

    chore(CriminalIntent): Ch14 CrimeListFragment에 AppBar 설정
    - Icon, Title 지정

--- CriminalIntent/app/src/main/java/com/tnsy/criminalintent/CrimeListFragment.kt
+++ CriminalIntent/app/src/main/java/com/tnsy/criminalintent/CrimeListFragment.kt
@@ -3,9 +3,7 @@ package com.tnsy.criminalintent
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
@@ -38,6 +36,11 @@ class CrimeListFragment : Fragment() {
         callbacks = context as Callbacks?
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -65,6 +68,11 @@ class CrimeListFragment : Fragment() {
     override fun onDetach() {
         super.onDetach()
         callbacks = null
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.fragment_crime_list, menu)
     }
 
     private fun updateUI(crimes: List<Crime>) {
