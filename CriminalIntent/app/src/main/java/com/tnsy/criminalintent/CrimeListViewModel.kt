07b98d3ed1fca18d6b0ae3eda396ea8cb7fb0f2c
suyeon <soo5717@sookmyung.ac.kr>
2021-08-01 17:12:52 +0900

    feat(CriminalIntent): Ch11 CrimeListViewModel에서 Repository 액세스
    - 테스트 데이터베이스 업로드

--- CriminalIntent/app/src/main/java/com/tnsy/criminalintent/CrimeListViewModel.kt
+++ CriminalIntent/app/src/main/java/com/tnsy/criminalintent/CrimeListViewModel.kt
@@ -3,14 +3,6 @@ package com.tnsy.criminalintent
 import androidx.lifecycle.ViewModel
 
 class CrimeListViewModel : ViewModel() {
-    val crimes = mutableListOf<Crime>()
-
-    init { // 테스트를 위한 임의의 데이터
-        for (i in 0 until 100) {
-            val crime = Crime()
-            crime.title = "Crime #$i"
-            crime.isSolved = i % 2 == 0
-            crimes += crime
-        }
-    }
+    private val crimeRepository = CrimeRepository.get()
+    val crimes = crimeRepository.getCrimes()
 }
